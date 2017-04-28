985b47bb8e383ac68e5eea5125e96e54b1a8ff38
Ayvytr <ayvytr@outlook.com>
2017-04-28 18:40:40 +0800

    1.更新MainActivity

--- app/src/main/java/com/ayvytr/dbspviewer/view/activity/MainActivity.java
+++ app/src/main/java/com/ayvytr/dbspviewer/view/activity/MainActivity.java
@@ -6,11 +6,16 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,6 +35,7 @@ import com.ayvytr.easyandroid.tools.FileTool;
 import com.ayvytr.easyandroid.tools.withcontext.Packages;
 import com.ayvytr.easyandroid.tools.withcontext.ToastTool;
 import com.ayvytr.root.Roots;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -49,6 +55,7 @@ import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
 {
     public static final String EXTRA_DB_FILEPATH = "extra_db_filepath";
     public static final String EXTRA_SP_APP_INFO = "extra_sp_app_info";
@@ -57,6 +64,14 @@ public class MainActivity extends AppCompatActivity
     RecyclerView recyclerView;
     @BindView(R.id.refreshLayout)
     SwipeRefreshLayout refreshLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.search_view)
+    MaterialSearchView searchView;
     private AppAdapter appAdapter;
 
     private int currentFilterType;
@@ -66,7 +81,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.AppTheme);
+        setTheme(R.style.NoActionbar);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         init();
@@ -100,6 +115,9 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_id_search);
+        searchView.setMenuItem(item);
         return true;
     }
 
@@ -108,14 +126,14 @@ public class MainActivity extends AppCompatActivity
     {
         switch(item.getItemId())
         {
-            case R.id.menu_id_help:
-                showHelpInfo();
-                return true;
             case R.id.menu_id_filter:
                 filterApplications();
                 break;
             case R.id.menu_id_sort:
                 sortApplications();
+                break;
+            case R.id.menu_id_search:
+
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -175,12 +193,17 @@ public class MainActivity extends AppCompatActivity
                     @Override
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which)
                     {
-                        Intent intent = new Intent(Intent.ACTION_VIEW);
-                        intent.setData(Uri.parse("https://github.com/ayvytr"));
-                        startActivity(intent);
+                        seeGitHub();
                     }
                 })
                 .show();
+    }
+
+    private void seeGitHub()
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://github.com/ayvytr"));
+        startActivity(intent);
     }
 
     private void initView()
@@ -199,6 +222,74 @@ public class MainActivity extends AppCompatActivity
                 appAdapter.update();
             }
         });
+
+        setSupportActionBar(toolbar);
+
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        toolbar.setNavigationIcon(R.drawable.launcher_transparent_small);
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+        navView.setNavigationItemSelectedListener(this);
+
+        searchView.setVoiceSearch(false);
+        searchView.setCursorDrawable(R.drawable.color_cursor_white);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                ToastTool.show("搜索");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                fillSuggestions(newText);
+                return false;
+            }
+        });
+
+//        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener()
+//        {
+//            @Override
+//            public void onSearchViewShown()
+//            {
+//                //Do some magic
+//            }
+//
+//            @Override
+//            public void onSearchViewClosed()
+//            {
+//                //Do some magic
+//            }
+//        });
+    }
+
+    private void fillSuggestions(String text)
+    {
+        List<AppInfo> list = appAdapter.getList();
+        List<String> strs = new ArrayList<>();
+        for(AppInfo appInfo : list)
+        {
+            if(appInfo.label != null && appInfo.label.contains(text))
+            {
+                strs.add(appInfo.label);
+            }
+            if(appInfo.className != null && appInfo.className.contains(text))
+            {
+                strs.add(appInfo.className);
+            }
+            if(appInfo.packageName != null && appInfo.packageName.contains(text))
+            {
+                strs.add(appInfo.packageName);
+            }
+        }
+
+        searchView.setSuggestions(list.toArray(new String[0]));
     }
 
     private void onSortApplications(List<AppInfo> list)
@@ -215,10 +306,31 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.nav_help:
+                showHelpInfo();
+                break;
+            case R.id.nav_view:
+                seeGitHub();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
     public class AppAdapter extends RecyclerView.Adapter<AppAdapter.Vh>
     {
         private List<AppInfo> list = new ArrayList<>();
+
+        public List<AppInfo> getList()
+        {
+            return list;
+        }
 
         @Override
         public Vh onCreateViewHolder(ViewGroup parent, int viewType)
@@ -262,27 +374,28 @@ public class MainActivity extends AppCompatActivity
                       .subscribeOn(Schedulers.io())
                       .buffer(1000)
                       .flatMap(new Function<List<AppInfo>, Observable<DiffUtil.DiffResult>>()
-                               {
-                                   @Override
-                                   public Observable<DiffUtil.DiffResult> apply(
-                                           final List<AppInfo> value) throws Exception
-                                   {
-                                       return new Observable<DiffUtil.DiffResult>()
-                                       {
-                                           @Override
-                                           protected void subscribeActual(
-                                                   Observer<? super DiffUtil.DiffResult> observer)
-                                           {
-                                               onSortApplications(value);
-                                               DiffUtil.DiffResult diffResult = DiffUtil
-                                                       .calculateDiff(new AppCallback(list, value), true);
-                                               observer.onNext(diffResult);
-                                               list = value;
-                                               observer.onComplete();
-                                           }
-                                       };
-                                   }
-                               }).subscribe(new Consumer<DiffUtil.DiffResult>() {
+                      {
+                          @Override
+                          public Observable<DiffUtil.DiffResult> apply(
+                                  final List<AppInfo> value) throws Exception
+                          {
+                              return new Observable<DiffUtil.DiffResult>()
+                              {
+                                  @Override
+                                  protected void subscribeActual(
+                                          Observer<? super DiffUtil.DiffResult> observer)
+                                  {
+                                      onSortApplications(value);
+                                      DiffUtil.DiffResult diffResult = DiffUtil
+                                              .calculateDiff(new AppCallback(list, value), true);
+                                      observer.onNext(diffResult);
+                                      list = value;
+                                      observer.onComplete();
+                                  }
+                              };
+                          }
+                      }).subscribe(new Consumer<DiffUtil.DiffResult>()
+            {
                 @Override
                 public void accept(DiffUtil.DiffResult diffResult) throws Exception
                 {
@@ -291,33 +404,6 @@ public class MainActivity extends AppCompatActivity
                     recyclerView.scrollToPosition(0);
                 }
             });
-//                      }).subscribe(new Observer<DiffUtil.DiffResult>()
-//            {
-//                @Override
-//                public void onSubscribe(Disposable d)
-//                {
-//                    refreshLayout.setRefreshing(true);
-//                }
-//
-//                @Override
-//                public void onNext(DiffUtil.DiffResult value)
-//                {
-//                    value.dispatchUpdatesTo(appAdapter);
-//                }
-//
-//                @Override
-//                public void onError(Throwable e)
-//                {
-//                    refreshLayout.setRefreshing(false);
-//                }
-//
-//                @Override
-//                public void onComplete()
-//                {
-//                    refreshLayout.setRefreshing(false);
-//                    recyclerView.scrollToPosition(0);
-//                }
-//            });
         }
 
         public class Vh extends RecyclerView.ViewHolder
